ba49a5268ff2fd598c07f491e1867c2b02ef4952
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-12-10 06:58:57 +0530

    Update article.dto.js

--- server/dtos/article.dto.js
+++ server/dtos/article.dto.js
@@ -1,5 +1,8 @@
 exports.GetTopArticlesResDTO = (result, message="Top Articles Fetched Successfully") => ({ success: true, result, message })
 
+exports.SaveArticleDTO = (token, article) => ({ token, article })
+exports.SaveArticleResDTO = (message="Article Saved Successfull") => ({ success: true, message})
+
 exports.ErrorResDTO = (message = "Something went wrong") => ({
     success: false,
     message
