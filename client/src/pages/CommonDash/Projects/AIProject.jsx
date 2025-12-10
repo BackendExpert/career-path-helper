a61be4041b3e2eefff368fe186bdda3e42d3cd7d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-12-10 11:33:41 +0530

    Update AIProject.jsx

--- client/src/pages/CommonDash/Projects/AIProject.jsx
+++ client/src/pages/CommonDash/Projects/AIProject.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import useForm from '../../../hooks/useForm'
+import API from '../../../services/api'
+import DefaultButton from '../../../component/Buttons/DefaultButton'
 
 const AIProject = () => {
+    const token = localStorage.getItem('token')
+    const [loading, setLoading] = useState(null)
+
+
     return (
-        <div>AIProject</div>
+        <div>
+            <div className="bg-white p-4 rounded-xl shadow-md">
+                <div className="mb-4 text-gray-500 ">
+                    Using you Current Skills Genarate a Project
+                </div>
+                <form action="" method="post">
+                    <div className="md:w-1/6">
+                        <DefaultButton
+                            type='submit'
+                            label={loading ? 'Genarating Projects' : 'Genarate a Project'}
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
 
