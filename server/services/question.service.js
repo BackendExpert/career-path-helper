a910da6c1ee85f81ae20a5c48213ec081c42438e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-12-11 07:46:56 +0530

    Update

---
+++ server/services/question.service.js
@@ -0,0 +1,17 @@
+const User = require("../models/user.model")
+const Member = require("../models/member.model")
+const Project = require("../models/project.model");
+const Skill = require("../models/skill.model")
+const AIProject = require("../models/aiproject.model")
+
+const logUserAction = require("../utils/others/logUserAction");
+
+const jwt = require("jsonwebtoken")
+
+class QuestionService {
+    static async getquestions () {
+        
+    }
+}
+
+module.exports = QuestionService
